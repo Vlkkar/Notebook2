04c2959ca7ac7c1c1158da356d8a59ff85e9fad2
Vlkkar <98618926+Vlkkar@users.noreply.github.com>
2023-06-06 14:50:53 +0700

    Update notes.cs

--- Notebook2/ViewModel/notes.cs
+++ Notebook2/ViewModel/notes.cs
@@ -6,12 +6,13 @@ namespace Notebook2.ViewModel
     public class notes
     {
         [Key] public int idnote { get; set; }
-        public int iduser { get; set; }
+        public int iduser { get; set; }        
 
+        [Required(ErrorMessage = "Поле обязательное для заполнения!")]
         [Display(Name = "Запись")]
         public string? note { get; set; }
 
-        [Required(ErrorMessage ="Поле Дата обязательное для заполнения")]
+        [Required(ErrorMessage ="Введите дату")]
         [Display(Name = "Дата")]
         public DateTime date { get; set; }
 
